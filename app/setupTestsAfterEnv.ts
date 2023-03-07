58e127d452290e5646f895d0530f0e43b8b10a00
Drewrey Lupton <drewrey@users.noreply.github.com>
2023-03-07 14:36:41 -0500

    Remove this since the stats test is in a node context, not a jsdom one

--- app/setupTestsAfterEnv.ts
+++ app/setupTestsAfterEnv.ts
@@ -36,13 +36,18 @@ import * as util from 'util';
 
 // ref: https://jestjs.io/docs/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
 // ref: https://github.com/jsdom/jsdom/issues/2524
-Object.defineProperty(window, 'TextEncoder', {
-  writable: true,
-  value: util.TextEncoder,
-});
-Object.defineProperty(window, 'TextDecoder', {
-  writable: true,
-  value: util.TextDecoder,
-});
+
+global.TextEncoder = util.TextEncoder;
+// @ts-ignore
+global.TextDecoder = util.TextDecoder;
+
+// Object.defineProperty(window, 'TextEncoder', {
+//   writable: true,
+//   value: util.TextEncoder,
+// });
+// Object.defineProperty(window, 'TextDecoder', {
+//   writable: true,
+//   value: util.TextDecoder,
+// });
 
 jest.setTimeout(40000);
